5e43a66b23e8a3a28849e778938fca09241b3c21
zxdmhn <2861123614@qq.com>
2018-09-29 09:51:46 +0800

    学生作业

--- Sort/main.c
+++ Sort/main.c
@@ -8,8 +8,29 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    printf("Hello, World!\n");
+    int a[10],i,j,k;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                k=a[j];
+                a[j]=a[i];
+                a[i]=k;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",a[i]);
+    }
     return 0;
 }
